f04d0db876903e51f79079b8c62ee56f938fd628
eslam <eslamfgazy600@gmail.com>
2023-01-02 12:55:13 +0200

    FoodRestaurant

--- androidApp/src/main/java/com/doubleclick/foodresturent/android/Home/ui/ProfileFragment.kt
+++ androidApp/src/main/java/com/doubleclick/foodresturent/android/Home/ui/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.doubleclick.foodresturent.android.Home.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.doubleclick.foodresturent.android.HistoryActivity
+import com.doubleclick.foodresturent.android.OffersActivity
+import com.doubleclick.foodresturent.android.OrdersActivity
 import com.doubleclick.foodresturent.android.R
 import com.doubleclick.foodresturent.android.databinding.FragmentProfileBinding
 
@@ -30,6 +34,18 @@ class ProfileFragment : Fragment() {
         binding.editPen.setOnClickListener {
             val editNameFragment = EditNameFragment();
             editNameFragment.show(requireActivity().supportFragmentManager, "Edit");
+        }
+
+        binding.offerCard.setOnClickListener {
+            startActivity(Intent(requireActivity(), OffersActivity::class.java))
+        }
+
+        binding.historyCard.setOnClickListener {
+            startActivity(Intent(requireActivity(), HistoryActivity::class.java))
+        }
+
+        binding.ordersCard.setOnClickListener {
+            startActivity(Intent(requireActivity(), OrdersActivity::class.java))
         }
     }
 }
