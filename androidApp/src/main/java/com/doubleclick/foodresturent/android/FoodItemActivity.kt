aafa37680657fa1b9b5f0252a8d9289f726b9df6
eslam <eslamfgazy600@gmail.com>
2023-01-11 21:12:34 +0200

    FoodRestaurant

---
+++ androidApp/src/main/java/com/doubleclick/foodresturent/android/FoodItemActivity.kt
@@ -0,0 +1,13 @@
+package com.doubleclick.foodresturent.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_item)
+
+    }
+}
